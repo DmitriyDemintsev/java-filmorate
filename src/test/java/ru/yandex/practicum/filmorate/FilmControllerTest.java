616e592f39e03a7b415a4ea67b4d7c3815a56f87
DmitriyDemintsev <d.a.demintsev@yandex.ru>
2023-10-01 22:37:33 +0300

    Поправил форматирование

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -25,88 +25,80 @@ public class FilmControllerTest {
 
     @Test
     void checkGetFilms() {
-        LocalDate releaseDate_1 = LocalDate.of(1997, 3, 24);
-        LocalDate releaseDate_2 = LocalDate.of(2004, 7, 03);
-        Film film_1 = new Film(1, "Фильм_1", "Описание_1", releaseDate_1, 120);
-        Film film_2 = new Film(2, "Фильм_2", "Описание_2", releaseDate_2, 180);
+        LocalDate releaseDate1 = LocalDate.of(1997, 3, 24);
+        LocalDate releaseDate2 = LocalDate.of(2004, 7, 03);
+        Film film1 = new Film(1, "Фильм_1", "Описание_1", releaseDate1, 120);
+        Film film2 = new Film(2, "Фильм_2", "Описание_2", releaseDate2, 180);
 
-        filmController.create(film_1);
-        filmController.create(film_2);
+        filmController.create(film1);
+        filmController.create(film2);
 
         List<Film> films = filmController.findAll();
-        assertEquals(films, List.of(film_1, film_2));
+        assertEquals(films, List.of(film1, film2));
     }
 
     @Test
     void checkCreateFilm() {
-        LocalDate releaseDate_1 = LocalDate.of(1895, 12, 28);
-        LocalDate releaseDate_2 = LocalDate.of(1894, 7, 03);
-        Film film_1 = new Film(1, "Фильм_1", "Описание_1", releaseDate_1, 120);
-        Film film_2 = new Film(2, null, "Описание_2", releaseDate_1, 180);
-        Film film_3 = new Film(3, "Фильм_3", "Сортировка вставками — достаточно простой алгоритм. " +
-                "Как в и любом другом, с увеличением размера сортируемого массива увеличивается и время сортировки. " +
-                "Это предложение превосходит размер в 200 знаков на 14 символов.", releaseDate_1, 210);
-        Film film_4 = new Film(4, "Фильм_4", "Описание_4", releaseDate_2, 240);
-        Film film_5 = new Film(5, "Фильм_5", "Описание_5", releaseDate_1, -273);
+        LocalDate releaseDate1 = LocalDate.of(1895, 12, 28);
+        LocalDate releaseDate2 = LocalDate.of(1894, 7, 03);
+        Film film1 = new Film(1, "Фильм_1", "Описание_1", releaseDate1, 120);
+        Film film2 = new Film(2, null, "Описание_2", releaseDate1, 180);
+        Film film3 = new Film(3, "Фильм_3", "Сортировка вставками — достаточно простой алгоритм. " + "Как в и любом другом, с увеличением размера сортируемого массива увеличивается и время сортировки. " + "Это предложение превосходит размер в 200 знаков на 14 символов.", releaseDate1, 210);
+        Film film4 = new Film(4, "Фильм_4", "Описание_4", releaseDate2, 240);
+        Film film5 = new Film(5, "Фильм_5", "Описание_5", releaseDate1, -273);
 
-        filmController.create(film_1);
-        ValidationException exception = assertThrows(ValidationException.class,
-                new Executable() {
-                    @Override
-                    public void execute() throws ValidationException {
-                        filmController.create(film_2);
-                    }
-                });
-        exception = assertThrows(ValidationException.class,
-                new Executable() {
-                    @Override
-                    public void execute() throws ValidationException {
-                        filmController.create(film_3);
-                    }
-                });
-        exception = assertThrows(ValidationException.class,
-                new Executable() {
-                    @Override
-                    public void execute() throws ValidationException {
-                        filmController.create(film_4);
-                    }
-                });
-        exception = assertThrows(ValidationException.class,
-                new Executable() {
-                    @Override
-                    public void execute() throws ValidationException {
-                        filmController.create(film_5);
-                    }
-                });
+        filmController.create(film1);
+        assertThrows(ValidationException.class, new Executable() {
+            @Override
+            public void execute() throws ValidationException {
+                filmController.create(film2);
+            }
+        });
+        assertThrows(ValidationException.class, new Executable() {
+            @Override
+            public void execute() throws ValidationException {
+                filmController.create(film3);
+            }
+        });
+        assertThrows(ValidationException.class, new Executable() {
+            @Override
+            public void execute() throws ValidationException {
+                filmController.create(film4);
+            }
+        });
+        assertThrows(ValidationException.class, new Executable() {
+            @Override
+            public void execute() throws ValidationException {
+                filmController.create(film5);
+            }
+        });
     }
 
     @Test
     void checkPutFilm() {
-        LocalDate releaseDate_1 = LocalDate.of(1997, 3, 24);
-        LocalDate releaseDate_2 = LocalDate.of(2012, 7, 03);
-        Film film_1 = new Film(1, "Фильм_1", "Описание_1", releaseDate_1, 120);
-        Film film_2 = new Film(2, "Фильм_2", "Описание_2", releaseDate_1, 180);
-        filmController.create(film_1);
-        //filmController.create(film_2);
+        LocalDate releaseDate1 = LocalDate.of(1997, 3, 24);
+        LocalDate releaseDate2 = LocalDate.of(2012, 7, 03);
+        Film film1 = new Film(1, "Фильм_1", "Описание_1", releaseDate1, 120);
+        Film film2 = new Film(2, "Фильм_2", "Описание_2", releaseDate1, 180);
+        filmController.create(film1);
 
-        film_1.setName("Обновили фильм");
-        film_1.setDescription("Обновили описание");
-        film_1.setReleaseDate(releaseDate_2);
-        film_1.setDuration(180);
+        film1.setName("Обновили фильм");
+        film1.setDescription("Обновили описание");
+        film1.setReleaseDate(releaseDate2);
+        film1.setDuration(180);
 
-        filmController.put(film_1);
-        assertEquals(film_1.getName(), "Обновили фильм");
-        assertEquals(film_1.getDescription(), "Обновили описание");
-        assertEquals(film_1.getReleaseDate(), releaseDate_2);
-        assertEquals(film_1.getDuration(), 180);
+        filmController.put(film1);
+        assertEquals(film1.getName(), "Обновили фильм");
+        assertEquals(film1.getDescription(), "Обновили описание");
+        assertEquals(film1.getReleaseDate(), releaseDate2);
+        assertEquals(film1.getDuration(), 180);
 
-        ValidationException exception = assertThrows(ValidationException.class,
-                new Executable() {
-                    @Override
-                    public void execute() throws ValidationException {
-                        filmController.put(film_2);
-                    }
-                });
-        assertNotNull(film_2, "Фильм не обновлен");
+        assertThrows(ValidationException.class, new Executable() {
+            @Override
+            public void execute() throws ValidationException {
+                filmController.put(film2);
+            }
+        });
+        assertNotNull(film2, "Фильм не обновлен");
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -27,106 +27,105 @@ public class UserControllerTest {
 
     @Test
     void checkGetUsers() throws UserAlreadyExistException {
-        LocalDate birthday_1 = LocalDate.of(1943, 1, 1);
-        LocalDate birthday_2 = LocalDate.of(1954, 12, 31);
-        User user_1 = new User(1, "test@test_1.com", "try_test_1", "Jim Beam", birthday_1);
-        User user_2 = new User(2, "test@test_2.com", "try_test_2", "John Donne", birthday_2);
+        LocalDate birthday1 = LocalDate.of(1943, 1, 1);
+        LocalDate birthday2 = LocalDate.of(1954, 12, 31);
+        User user1 = new User(1, "test@test_1.com", "try_test_1", "Jim Beam", birthday1);
+        User user2 = new User(2, "test@test_2.com", "try_test_2", "John Donne", birthday2);
 
-        userController.create(user_1);
-        userController.create(user_2);
+        userController.create(user1);
+        userController.create(user2);
 
         List<User> users = userController.findUsers();
-        assertEquals(users, List.of(user_1, user_2));
+        assertEquals(users, List.of(user1, user2));
     }
 
     @Test
     void checkCreateUser() throws UserAlreadyExistException {
-        LocalDate birthday_1 = LocalDate.now();
-        LocalDate birthday_2 = LocalDate.of(2243, 1, 1);
-        LocalDate birthday_3 = LocalDate.of(1946, 8, 20);
-        User user_1 = new User(1, "test@test_1.com", "try_test_1", "Jim Beam", birthday_1);
-        User user_2 = new User(2, "test@test_2.com", "try_test_2", null, birthday_1);
-        User user_3 = new User(3, null, "try_test_3", "Jim Beam", birthday_1);
-        User user_4 = new User(4, "testtest_4.com", "try_test_4", "Jim Beam", birthday_1);
-        User user_5 = new User(5, "test@test_5.com", null, "Jim Beam", birthday_1);
-        User user_6 = new User(6, "test@test_6.com", "try test_6", "Jim Beam", birthday_1);
-        User user_7 = new User(7, "test@test_7.com", "try_test_7", "Jim Beam", birthday_2);
-        User user_8 = new User(8, "test@test_1.com", "try_test_8", "Jim NickName", birthday_3);
+        LocalDate birthday1 = LocalDate.now();
+        LocalDate birthday2 = LocalDate.of(2243, 1, 1);
+        LocalDate birthday3 = LocalDate.of(1946, 8, 20);
+        User user1 = new User(1, "test@test_1.com", "try_test_1", "Jim Beam", birthday1);
+        User user2 = new User(2, "test@test_2.com", "try_test_2", null, birthday1);
+        User user3 = new User(3, null, "try_test_3", "Jim Beam", birthday1);
+        User user4 = new User(4, "testtest_4.com", "try_test_4", "Jim Beam", birthday1);
+        User user5 = new User(5, "test@test_5.com", null, "Jim Beam", birthday1);
+        User user6 = new User(6, "test@test_6.com", "try test_6", "Jim Beam", birthday1);
+        User user7 = new User(7, "test@test_7.com", "try_test_7", "Jim Beam", birthday2);
+        User user8 = new User(8, "test@test_1.com", "try_test_8", "Jim NickName", birthday3);
 
-        userController.create(user_1);
-        userController.create(user_2);
-        final InvalidEmailException emailException = assertThrows(InvalidEmailException.class,
+        userController.create(user1);
+        userController.create(user2);
+        assertThrows(InvalidEmailException.class,
                 new Executable() {
                     @Override
                     public void execute() throws UserAlreadyExistException {
-                        userController.create(user_3);
+                        userController.create(user3);
                     }
                 });
-        ValidationException exception = assertThrows(ValidationException.class,
+        assertThrows(ValidationException.class,
                 new Executable() {
                     @Override
                     public void execute() throws UserAlreadyExistException {
-                        userController.create(user_4);
+                        userController.create(user4);
                     }
                 });
 
-        exception = assertThrows(ValidationException.class,
+        assertThrows(ValidationException.class,
                 new Executable() {
                     @Override
                     public void execute() throws UserAlreadyExistException {
-                        userController.create(user_8);
+                        userController.create(user8);
                     }
                 });
 
-        exception = assertThrows(ValidationException.class,
+        assertThrows(ValidationException.class,
                 new Executable() {
                     @Override
                     public void execute() throws UserAlreadyExistException {
-                        userController.create(user_5);
+                        userController.create(user5);
                     }
                 });
 
-        exception = assertThrows(ValidationException.class,
+        assertThrows(ValidationException.class,
                 new Executable() {
                     @Override
                     public void execute() throws UserAlreadyExistException {
-                        userController.create(user_6);
+                        userController.create(user6);
                     }
                 });
 
-        exception = assertThrows(ValidationException.class,
+        assertThrows(ValidationException.class,
                 new Executable() {
                     @Override
                     public void execute() throws UserAlreadyExistException {
-                        userController.create(user_7);
+                        userController.create(user7);
                     }
                 });
 
-
-        assertNotNull(user_1, "Пользователь не создан");
-        assertEquals(user_2.getName(), user_2.getLogin());
-        assertNotNull(user_4, "Пользователь с Email, не содержащим @, не создан");
-        assertNotNull(user_5, "Пользователь с пустым логином не создан");
-        assertNotNull(user_6, "Пользователь с логином, содержащим пробелы, не создан");
-        assertNotNull(user_7, "Пользователь с невалидной датой рождения не создан");
+        assertNotNull(user1, "Пользователь не создан");
+        assertEquals(user2.getName(), user2.getLogin());
+        assertNotNull(user4, "Пользователь с Email, не содержащим @, не создан");
+        assertNotNull(user5, "Пользователь с пустым логином не создан");
+        assertNotNull(user6, "Пользователь с логином, содержащим пробелы, не создан");
+        assertNotNull(user7, "Пользователь с невалидной датой рождения не создан");
     }
 
     @Test
     void checkPutUser() throws UserAlreadyExistException {
-        LocalDate birthday_1 = LocalDate.of(1943, 1, 1);
-        LocalDate birthday_2 = LocalDate.of(2017, 3, 23);
-        User user = new User(1, "test@test.com", "oldLogin", "Jim Beam", birthday_1);
+        LocalDate birthday1 = LocalDate.of(1943, 1, 1);
+        LocalDate birthday2 = LocalDate.of(2017, 3, 23);
+        User user = new User(1, "test@test.com", "oldLogin", "Jim Beam", birthday1);
         userController.create(user);
 
         user.setEmail("test@test.com");
         user.setLogin("newLogin");
         user.setName("John Donne");
-        user.setBirthday(birthday_2);
+        user.setBirthday(birthday2);
 
         userController.put(user);
         assertEquals(user.getEmail(), "test@test.com");
         assertEquals(user.getLogin(), "newLogin");
         assertEquals(user.getName(), "John Donne");
-        assertEquals(user.getBirthday(), birthday_2);
+        assertEquals(user.getBirthday(), birthday2);
     }
 }
